92be866ce7d2175160f90040cdeef0654d981eb4
Olivoz <lucas.sivars@gmail.com>
2022-09-20 11:39:51 +0200

    Append customer to table

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,28 @@
-function build() {}
+function appendElement(target, elementName) {
+  let element = document.createElement(elementName);
+  return target.appendChild(element);
+}
+
+function appendCustomerInfo(target, value) {
+  let tag = document.createElement("td");
+  tag.innerText = value;
+  target.appendChild(tag);
+}
+
+function appendCustomer(target, name, number, adress) {
+  let customerTag = document.createElement("tr");
+  appendCustomerInfo(customerTag, name);
+  appendCustomerInfo(customerTag, number);
+  appendCustomerInfo(customerTag, adress);
+  target.appendChild(customerTag);
+}
+
+function build() {
+  addButton.onclick = () => {
+    let name = nameInput.value;
+    let number = telInput.value;
+    let adress = adressInput.value;
+    if (!name || !number || !adress) return;
+    appendCustomer(customerTable, name, number, adress);
+  };
+}
